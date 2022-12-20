1f3a6642d0945f47e20c5352acf16fc937263087
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-12-20 19:19:43 +0530

    Add validation exception handler

--- src/main/java/lk/ijse/dep9/app/dto/UserDTO.java
+++ src/main/java/lk/ijse/dep9/app/dto/UserDTO.java
@@ -14,12 +14,12 @@ import java.io.Serializable;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDTO implements Serializable {
-    @NotBlank
-    @Pattern(regexp = "^[A-Za-z ]+$")
+    @NotBlank(message = "Full name can't be empty or null")
+    @Pattern(regexp = "^[A-Za-z ]+$", message = "Invalid name")
     private String fullName;
-    @NotBlank
+    @NotBlank(message = "Username can't be empty or null")
     private String username;
-    @NotEmpty
-    @Length(min = 3)
+    @NotEmpty(message = "Password can't be empty or null")
+    @Length(min = 3, message = "Password should be at least 3 characters long")
     private String password;
 }
