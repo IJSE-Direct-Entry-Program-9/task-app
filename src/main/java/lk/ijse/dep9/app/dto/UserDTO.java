574dcceeda3946d7b8b4a0c16ffcfe04d911370a
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-12-22 13:28:10 +0530

    Finalize updateUserAccountDetails web service

--- src/main/java/lk/ijse/dep9/app/dto/UserDTO.java
+++ src/main/java/lk/ijse/dep9/app/dto/UserDTO.java
@@ -10,25 +10,22 @@ import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Null;
 import javax.validation.constraints.Pattern;
 import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-//@JsonIgnoreProperties(value = "password", allowSetters = true)
+@JsonIgnoreProperties(value = "password", allowSetters = true)
 public class UserDTO implements Serializable {
     @NotBlank(message = "Full name can't be empty or null")
     @Pattern(regexp = "^[A-Za-z ]+$", message = "Invalid name")
     private String fullName;
+    @Null(groups = ValidationGroups.Update.class, message = "Username can't be updated")
     @NotBlank(message = "Username can't be empty or null", groups = ValidationGroups.Create.class)
     private String username;
     @NotEmpty(message = "Password can't be empty or null")
     @Length(min = 3, message = "Password should be at least 3 characters long")
     private String password;
-
-    @JsonIgnore
-    public String getPassword() {
-        return password;
-    }
 }
