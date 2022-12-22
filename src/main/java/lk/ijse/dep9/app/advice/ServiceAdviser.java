c09392c3901fda25216cc8bcf8402996d6851ffc
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-12-22 18:17:02 +0530

    Assert completed 2

--- src/main/java/lk/ijse/dep9/app/advice/ServiceAdviser.java
+++ src/main/java/lk/ijse/dep9/app/advice/ServiceAdviser.java
@@ -5,7 +5,6 @@ import lk.ijse.dep9.app.dao.custom.TaskDAO;
 import lk.ijse.dep9.app.dto.ProjectDTO;
 import lk.ijse.dep9.app.dto.TaskDTO;
 import lk.ijse.dep9.app.entity.Project;
-import lk.ijse.dep9.app.entity.Task;
 import lk.ijse.dep9.app.exception.AccessDeniedException;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.annotation.Aspect;
@@ -21,42 +20,42 @@ import org.springframework.web.server.ResponseStatusException;
 @Slf4j
 public class ServiceAdviser {
 
-    private ProjectDAO projectDAO;
-    private TaskDAO taskDAO;
+    private final ProjectDAO projectDAO;
+    private final TaskDAO taskDAO;
 
     public ServiceAdviser(ProjectDAO projectDAO, TaskDAO taskDAO) {
         this.projectDAO = projectDAO;
         this.taskDAO = taskDAO;
     }
 
-    @Pointcut("execution(public * lk.ijse.dep9.app.service.custom.ProjectTaskService(..))")
-    public void serviceMethodAuthorization(){}
+    @Pointcut("execution(public * lk.ijse.dep9.app.service.custom.ProjectTaskService.*(..))")
+    public void serviceMethodAuthorization() {
+    }
 
     @Before(value = "serviceMethodAuthorization() && args(username,projectId)",
             argNames = "username,projectId")
-    public void serviceMethodAuthorization(String username, int projectId){
+    public void serviceMethodAuthorization(String username, int projectId) {
         executeAdvice(username, projectId);
     }
 
     @Before(value = "serviceMethodAuthorization() && args(project)", argNames = "project")
-    public void serviceMethodAuthorization(ProjectDTO project){
+    public void serviceMethodAuthorization(ProjectDTO project) {
         if (project.getId() != null) executeAdvice(project.getUsername(), project.getId());
     }
 
     @Before(value = "serviceMethodAuthorization() && args(username, task, ..)", argNames = "username,task")
-    public void serviceMethodAuthorization(String username, TaskDTO task){
+    public void serviceMethodAuthorization(String username, TaskDTO task) {
         executeAdvice(username, task.getProjectId());
-        if (task.getId() != null){
-            Task taskEntity = taskDAO.findById(task.getId()).
-                    orElseThrow(() -> new EmptyResultDataAccessException(1));
-            if (taskDAO.findAllTasksByProjectId(taskEntity.getProjectId())
-                    .stream().noneMatch(t -> t.getId() == task.getId())){
+        if (task.getId() != null) {
+            taskDAO.findById(task.getId()).orElseThrow(() -> new EmptyResultDataAccessException(1));
+            if (taskDAO.findAllTasksByProjectId(task.getProjectId())
+                    .stream().noneMatch(t -> t.getId() == task.getId())) {
                 throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
             }
         }
     }
 
-    private void executeAdvice(String username, int projectId){
+    private void executeAdvice(String username, int projectId) {
         Project project = projectDAO.findById(projectId).orElseThrow(
                 () -> new EmptyResultDataAccessException(1));
         if (!project.getUsername().matches(username)) throw new AccessDeniedException();
